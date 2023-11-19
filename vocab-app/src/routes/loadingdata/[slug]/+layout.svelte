d9369d8ca5249ce70b56f90e4306ba3afd00481f
Koyena Pal <koyenapal12@gmail.com>
2023-11-19 01:01:18 -0500

    working loading data

---
+++ vocab-app/src/routes/loadingdata/[slug]/+layout.svelte
@@ -0,0 +1,1 @@
+<script>import "/src/app.postcss";</script><slot></slot>
---
+++ vocab-app/src/routes/loadingdata/[slug]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import papa from 'papaparse';
+
+  function handleChange(event: any) {
+      let files = event.target.files;
+      for (let f of files) {
+          papa.parse(f, {
+              header: true,
+              complete: function (results: any) {
+                  console.log(results);
+              }
+          });
+      }
+  }
+</script>
+
+
+
+Upload a csv of the measurements:
+
+<input type="file" on:change={handleChange} />
